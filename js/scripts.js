7a5186801b7c2062b9e31745c25f07656cefda6d
Diana <holla@bikefarm.org>
2015-09-14 12:42:58 -0700

    Add behavior for getLength()

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-var arraySize = function(input) {
-  var value = input.split('').length;
-  return value;
+// var arrayLength = function(input) {
+//   var length = input.replace(/\s+/g, '').split('').length;
+//   return length;
+// }
+
+
+var getArray = function(input) {
+  var charArray = input.replace(/\s+/g, '').split('');
+  return charArray;
+}
+
+var getLength = function(charArray) {
+  var arrayLength = charArray.length;
+  return arrayLength;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,14 @@
-describe('arraySize', function() { //first arg is a string and the name of the function to be built
-  it("has the same number of characters as the user input string", function() { //first arg is string descrip of behavior
-    expect(arraySize("It's party time")).to.equal(15);
+describe('getArray', function() {
+  it("is the user input without spaces", function() {
+    expect(getArray("It's party time")).to.eql(["I", "t", "'", "s", "p", "a"+
+    "", "r", "t", "y", "t", "i", "m", "e"]);
+  })
+})
+
+describe('getLength', function() { //first arg is a string and the name of the function to be built
+  it("is the length of the input without spaces", function() { //first arg is string descrip of behavior
+    expect(getLength(["I", "t", "'", "s", "p", "a"+
+    "", "r", "t", "y", "t", "i", "m", "e"])).to.equal(13);
   });
 
 });
