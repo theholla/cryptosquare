7212f8ccf52b4f1f03c78a346dd45aa5ff61f498
Diana <holla@bikefarm.org>
2015-09-14 15:48:25 -0700

    Refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
-var getArray = function(input) {
+function getArray(input) {
   var charArray = input.replace(/\W+/g, "").split('');
   return charArray;
 }
 
-var getLength = function(charArray) {
+function getCryptoLength(charArray) {
   var arrayLength = charArray.length;
-  return arrayLength;
+  var squareRoot = Math.sqrt(arrayLength);
+  var cryptoLength = Math.ceil(squareRoot);
+  return cryptoLength;
 }
+
+// function makeCryptoArrays(charArray, squareRoot) {
+//   while(charArray.length) {
+//     var cryptoArrays[] = charArray.splice(0, squareRoot);
+//     return cryptoArrays[];
+//   }
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,17 @@
 describe('getArray', function() {
-  it("is the user input without spaces", function() {
+  it("is the user input without nonword characters", function() {
     expect(getArray("It's party time")).to.eql(["I", "t", "s", "p", "a", "r", "t", "y", "t", "i", "m", "e"]);
   })
 })
 
-describe('getLength', function() { //first arg is a string and the name of the function to be built
-  it("is the length of the input without spaces", function() { //first arg is string descrip of behavior
-    expect(getLength(["I", "t", "s", "p", "a", "r", "t", "y", "t", "i", "m", "e"])).to.equal(12);
-  });
+describe('getCryptoLength', function() {
+  it("gets the length of each encrypted string", function() {
+    expect(getCryptoLength(["I", "t", "s", "p", "a", "r", "t", "y", "t", "i", "m", "e"])).to.equal(4);
+  })
+})
 
-});
+// describe('makeCryptoArrays', function() {
+//   it("makes the appropriate number of arrays", function() {
+//     expect(makeCryptoArrays(["I", "t", "s", "p", "a", "r", "t", "y", "t", "i", "m", "e"])).to.eql([ "I", "t", "s" ], [ "p", "a", "r" ], ["t", "y", "t"], ["i", "m", "e"]);
+//   })
+// })
