2d74dd574c9346df28f2ace05f5804ade778fe01
Diana <holla@bikefarm.org>
2015-09-14 17:07:29 -0700

    Add passing spec for getting crypto array

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
-function getArray(input) {
-  var charArray = input.replace(/\W+/g, "").split('');
-  return charArray;
+function getArray(userInput) {
+  var input = userInput.toLowerCase().replace(/\W+/g, "");
+  return input;
 }
 
-function getCryptoLength(charArray) {
-  var arrayLength = charArray.length;
-  var squareRoot = Math.sqrt(arrayLength);
+function getCryptoLength(input) {
+  var inputLength = input.length;
+  var squareRoot = Math.sqrt(inputLength);
   var cryptoLength = Math.ceil(squareRoot);
   return cryptoLength;
 }
 
-// function makeCryptoArrays(charArray, squareRoot) {
-//   while(charArray.length) {
-//     var cryptoArrays[] = charArray.splice(0, squareRoot);
-//     return cryptoArrays[];
-//   }
-// }
+function makeCryptoArray(input, cryptoLength) {
+  var columns = [];
+  for (var i = 0; i < input.length; i ++) {
+    if (i % cryptoLength == 0 && i != 0)
+    columns.push(input.substring(i - cryptoLength, i));
+    if (i == input.length - 1)
+    columns.push(input.substring(i - (i % cryptoLength), i+1));
+  }
+  return columns;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,17 @@
 describe('getArray', function() {
   it("is the user input without nonword characters", function() {
-    expect(getArray("It's party time")).to.eql(["I", "t", "s", "p", "a", "r", "t", "y", "t", "i", "m", "e"]);
+    expect(getArray("It's party time")).to.eql("itspartytime");
   })
 })
 
 describe('getCryptoLength', function() {
-  it("gets the length of each encrypted string", function() {
-    expect(getCryptoLength(["I", "t", "s", "p", "a", "r", "t", "y", "t", "i", "m", "e"])).to.equal(4);
+  it("gets the number of columns based on the square root", function() {
+    expect(getCryptoLength("itspartytime")).to.equal(4);
   })
 })
 
-// describe('makeCryptoArrays', function() {
-//   it("makes the appropriate number of arrays", function() {
-//     expect(makeCryptoArrays(["I", "t", "s", "p", "a", "r", "t", "y", "t", "i", "m", "e"])).to.eql([ "I", "t", "s" ], [ "p", "a", "r" ], ["t", "y", "t"], ["i", "m", "e"]);
-//   })
-// })
+describe('makeCryptoArray', function() {
+  it("makes the appropriate array", function() {
+    expect(makeCryptoArray("itspartytime", 4)).to.eql(["itsp", "arty", "time"]);
+  })
+})
